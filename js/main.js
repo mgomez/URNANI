91f54b098c90849d434fcc90f6189b3e6ab52953
Cesar Gomez <00.13.zkr@gmail.com>
2015-11-24 10:22:03 -0600

    RC 1

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var monthOffset = [6,7,8,9,10,11,0,1,2,3,4,5];
 var monthArray = [["ENE","enero"],["FEB","Febrero"],["MAR","Marzo"],["ABR","Abril"],["MAY","Mayo"],["JUN","Junio"],["JUL","Julio"],["AGO","Agosto"],["SEP","Septiembre"],["OCT","Octubre"],["NOV","Noviembre"],["DIC","Diciembre"]];
 var letrasArray = ["L","M","X","J","V","S","D"];
 var dayArray = ["7","1","2","3","4","5","6"];
-function nannisIndex(){
+function padresIndex(){
 	$(document).on('click','.calendar-day.have-events',activateDay);
 	$(document).on('click','.specific-day',activatecalendar);
 	$(document).on('click','.calendar-month-view-arrow',offsetcalendar);
@@ -41,18 +41,22 @@ function nannisIndex(){
 	calendarSet();
 	calendarScale();
 }
-function nannis(){}
-function nannisDisponibles(){
+function padres(){}
+function padresDisponibles(){
 	$(".nanni").on("click", function(){
-		goTo("nannisConocela");
+		goTo("padresConocela");
 	});
 }
-function nannisConocela(){}
+function padresConocela(){}
 function goTo(view){
     $("#app").hide();
     var url = "views/"+view+".html";
     $.get(url, function(html){
         $("#app").html(html).show();
-        eval(view)();               
+        try{
+        	eval(view)();               
+        }catch(e){
+        	console.warn(e);
+        }
     });
 }
